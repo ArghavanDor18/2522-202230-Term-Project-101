dfeb73102c6447e9133823c0e881a6f962e1fe0f
Arghavan Dor <97262547+ArghavanDor18@users.noreply.github.com>
2022-11-01 13:40:42 -0700

    Hello World!

---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module ca.bcit.comp2522.termproject._101 {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens ca.bcit.comp2522.termproject._101 to javafx.fxml;
+    exports ca.bcit.comp2522.termproject._101;
+}
