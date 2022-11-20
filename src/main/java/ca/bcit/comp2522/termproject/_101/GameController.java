55bbf3cdc5287ce5752fdb2b5d5431d2e72cf474
Kristopher Ocampo <kris.ocampo@hotmail.com>
2022-11-19 20:48:14 -0800

    -m Created and added instructions on how to play game in "GameController.java"

--- src/main/java/ca/bcit/comp2522/termproject/_101/GameController.java
+++ src/main/java/ca/bcit/comp2522/termproject/_101/GameController.java
@@ -14,11 +14,15 @@ public class GameController {
 
     @FXML
     protected final void onNewGameButtonClick() {
-        newGame.setText("");
+        newGame.setText("Starting Game....");
     }
 
     @FXML
     protected final void onInstructionsButtonClick(){
-        instructions.setText("");
+        instructions.setText("This fun game tests your memory, observation, and matching skills!\n"
+                + "\n" + "How to Play: The game starts with a grid of nine squares, with \n characters hiding underneath. "
+                + "Clicking a square reveals a letter or " + "\n a picture. " + "If you find a corresponding square with "
+                + "the same letter \n or picture, you have found a match!\n"
+                + "\n" + "Objective: Find all matching pairs to win the game");
     }
 }
