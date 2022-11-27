38b0cc9392b9e3d28fcfeda1b0cff06c7c27207a
Kristopher Ocampo <kris.ocampo@hotmail.com>
2022-11-27 12:40:21 -0800

    -m "In GameController.java: Continued to work on matching function, in addition to the button click functionality: revealing and hiding elements. "

--- src/main/java/ca/bcit/comp2522/termproject/_101/GameController.java
+++ src/main/java/ca/bcit/comp2522/termproject/_101/GameController.java
@@ -16,47 +16,99 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.ResourceBundle;
 
-public class GameController {
+public class GameController implements Initializable {
     @FXML
     private Label newGame;
 
     @FXML
     private Label instructions;
 
-//    ArrayList<Button> buttons = new ArrayList<>();
-//
-//    MemoryMatchingGame memoryMatchingGame = new MemoryMatchingGame();
-//
-//    @FXML
-//    private Button button_0;
-//    @FXML
-//    private Button button_1;
-//    @FXML
-//    private Button button_2;
-//    @FXML
-//    private Button button_3;
-//
-//    Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1.5), e -> hideButtons()));
-//
-//    private boolean firstButtonClicked = false;
-//
-//    private int indexOfFirstButton;
-//    private int indexofSecondButton;
-//    private boolean match;
+    ArrayList<Button> buttons = new ArrayList<>();
 
-
+    MemoryMatchingGame memoryMatchingGame = new MemoryMatchingGame();
 
     @FXML
-    protected final void onNewGameButtonClick() {
-        newGame.setText("Starting Game....");
+    private Button button_0;
+    @FXML
+    private Button button_1;
+    @FXML
+    private Button button_2;
+    @FXML
+    private Button button_3;
+
+    Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1.5), e -> hideButtons()));
+
+    // Hide the two clicked buttons should those two clicked buttons not be a match
+    private void hideButtons() {
+        buttons.get(indexOfFirstButton).setText("");
+        buttons.get(indexOfSecondButton).setText("");
     }
 
-    @FXML
-    protected final void onInstructionsButtonClick(){
-        instructions.setText("This fun game tests your memory, observation, and matching skills!\n"
-                + "\n" + "How to Play: The game starts with a grid of nine squares, with \n characters hiding underneath. "
-                + "Clicking a square reveals a letter or " + "\n a picture. " + "If you find a corresponding square with "
-                + "the same letter \n or picture, you have found a match!\n"
-                + "\n" + "Objective: Find all matching pairs to win the game");
-    }
+    private boolean firstButtonClicked = false;
+
+    private int indexOfFirstButton;
+    private int indexOfSecondButton;
+    private boolean match;
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        memoryMatchingGame.setupGame();
+
+        buttons.addAll(Arrays.asList(button_0, button_1, button_2, button_3));
+
+        @FXML
+        void buttonIsClicked(ActionEvent event){
+            if (!firstButtonClicked) {
+                //If the chosen button is not a match, hide the button
+                if (!match) {
+                    hideButtons();
+                    timeline.stop();
+                }
+                match = false;
+                firstButtonClicked = true;
+
+                //Reveal clicked button's letter
+                String buttonId = ((Control) event.getSource()).getId();
+                indexOfFirstButton = Integer.parseInt(buttonId.substring(buttonId.length() - 1));
+
+                //Change text of clicked button
+                buttons.get(indexOfFirstButton).setText(memoryMatchingGame.getOptionAtIndex(indexOfFirstButton));
+
+                return;
+            }
+
+            String buttonId = ((Control) event.getSource()).getId();
+            indexOfSecondButton = Integer.parseInt(buttonId.substring(buttonId.length() - 1));
+
+            //Change the text of clicked button
+            buttons.get(indexOfSecondButton).setText(memoryMatchingGame.getOptionAtIndex(indexOfSecondButton));
+
+            firstButtonClicked = false;
+
+            //Check if the two clicked buttons are a match
+            if (memoryMatchingGame.checkTwoPositionsInArray(indexOfFirstButton, indexOfSecondButton)) {
+                System.out.println("Match");
+                match = true;
+                return;
+            }
+
+            timeline.play();
+        }
+
+
+//        @FXML
+//        protected final void onNewGameButtonClick () {
+//            newGame.setText("Starting Game....");
+//        }
+//
+//        @FXML
+//        protected final void onInstructionsButtonClick () {
+//            instructions.setText("This fun game tests your memory, observation, and matching skills!\n"
+//                    + "\n" + "How to Play: The game starts with a grid of nine squares, with \n characters hiding underneath. "
+//                    + "Clicking a square reveals a letter or " + "\n a picture. " + "If you find a corresponding square with "
+//                    + "the same letter \n or picture, you have found a match!\n"
+//                    + "\n" + "Objective: Find all matching pairs to win the game");
+//        }
+//    }
+
 }
