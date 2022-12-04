07ee5022af6da3c4b8be6e65f21d40eb616c50cb
Kristopher Ocampo <kris.ocampo@hotmail.com>
2022-12-04 13:26:27 -0800

    -m "Reformatted GameController.java: Continued to work on matching function. Edited game-view.fxml to link buttons created in GameController.java "

--- src/main/java/ca/bcit/comp2522/termproject/_101/GameController.java
+++ src/main/java/ca/bcit/comp2522/termproject/_101/GameController.java
@@ -5,7 +5,7 @@ import javafx.scene.control.Label;
 import javafx.animation.KeyFrame;
 import javafx.animation.Timeline;
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
+
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
 import javafx.scene.control.Control;
@@ -15,6 +15,8 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.ResourceBundle;
+
+//import static javax.xml.catalog.CatalogMessages.resourceBundle;
 
 public class GameController implements Initializable {
     @FXML
@@ -38,11 +40,7 @@ public class GameController implements Initializable {
 
     Timeline timeline = new Timeline(new KeyFrame(Duration.seconds(1.5), e -> hideButtons()));
 
-    // Hide the two clicked buttons should those two clicked buttons not be a match
-    private void hideButtons() {
-        buttons.get(indexOfFirstButton).setText("");
-        buttons.get(indexOfSecondButton).setText("");
-    }
+
 
     private boolean firstButtonClicked = false;
 
@@ -51,64 +49,69 @@ public class GameController implements Initializable {
     private boolean match;
 
     @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    public void initialize (URL url, ResourceBundle resourceBundle){
         memoryMatchingGame.setupGame();
 
         buttons.addAll(Arrays.asList(button_0, button_1, button_2, button_3));
 
-        @FXML
-        void buttonIsClicked(ActionEvent event){
-            if (!firstButtonClicked) {
-                //If the chosen button is not a match, hide the button
-                if (!match) {
-                    hideButtons();
-                    timeline.stop();
-                }
-                match = false;
-                firstButtonClicked = true;
+    }
+    @FXML
 
-                //Reveal clicked button's letter
-                String buttonId = ((Control) event.getSource()).getId();
-                indexOfFirstButton = Integer.parseInt(buttonId.substring(buttonId.length() - 1));
-
-                //Change text of clicked button
-                buttons.get(indexOfFirstButton).setText(memoryMatchingGame.getOptionAtIndex(indexOfFirstButton));
-
-                return;
+    void buttonIsClicked(ActionEvent event) {
+        if (!firstButtonClicked) {
+            //If the chosen button is not a match, hide the button
+            if (!match) {
+                hideButtons();
+                timeline.stop();
             }
+            match = false;
+            firstButtonClicked = true;
 
+            //Reveal clicked button's letter
             String buttonId = ((Control) event.getSource()).getId();
-            indexOfSecondButton = Integer.parseInt(buttonId.substring(buttonId.length() - 1));
+            indexOfFirstButton = Integer.parseInt(buttonId.substring(buttonId.length() - 1));
 
-            //Change the text of clicked button
-            buttons.get(indexOfSecondButton).setText(memoryMatchingGame.getOptionAtIndex(indexOfSecondButton));
+            //Change text of clicked button
+            buttons.get(indexOfFirstButton).setText(memoryMatchingGame.getOptionAtIndex(indexOfFirstButton));
 
-            firstButtonClicked = false;
-
-            //Check if the two clicked buttons are a match
-            if (memoryMatchingGame.checkTwoPositionsInArray(indexOfFirstButton, indexOfSecondButton)) {
-                System.out.println("Match");
-                match = true;
-                return;
-            }
-
-            timeline.play();
+            return;
         }
 
+        String buttonId = ((Control) event.getSource()).getId();
+        indexOfSecondButton = Integer.parseInt(buttonId.substring(buttonId.length() - 1));
 
-//        @FXML
-//        protected final void onNewGameButtonClick () {
-//            newGame.setText("Starting Game....");
-//        }
-//
-//        @FXML
-//        protected final void onInstructionsButtonClick () {
-//            instructions.setText("This fun game tests your memory, observation, and matching skills!\n"
-//                    + "\n" + "How to Play: The game starts with a grid of nine squares, with \n characters hiding underneath. "
-//                    + "Clicking a square reveals a letter or " + "\n a picture. " + "If you find a corresponding square with "
-//                    + "the same letter \n or picture, you have found a match!\n"
-//                    + "\n" + "Objective: Find all matching pairs to win the game");
-//        }
-//    }
+        //Change the text of clicked button
+        buttons.get(indexOfSecondButton).setText(memoryMatchingGame.getOptionAtIndex(indexOfSecondButton));
 
-}
+        firstButtonClicked = false;
+
+        //Check if the two clicked buttons are a match
+        if (memoryMatchingGame.checkTwoPositionsInArray(indexOfFirstButton, indexOfSecondButton)) {
+            System.out.println("Match");
+            match = true;
+            return;
+        }
+
+        timeline.play();
+    }
+    // Hide the two clicked buttons should those two clicked buttons not be a match
+    private void hideButtons() {
+        buttons.get(indexOfFirstButton).setText("");
+        buttons.get(indexOfSecondButton).setText("");
+    }
+
+
+        @FXML
+        protected final void onNewGameButtonClick () {
+            newGame.setText("Starting Game....");
+        }
+
+        @FXML
+        protected final void onInstructionsButtonClick () {
+            instructions.setText("This fun game tests your memory, observation, and matching skills!\n"
+                    + "\n" + "How to Play: The game starts with a grid of nine squares, with \n characters hiding underneath. "
+                    + "Clicking a square reveals a letter or " + "\n a picture. " + "If you find a corresponding square with "
+                    + "the same letter \n or picture, you have found a match!\n"
+                    + "\n" + "Objective: Find all matching pairs to win the game");
+        }
+    }
